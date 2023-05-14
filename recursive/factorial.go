f0a0d840de028edd0ba9be32ed99caa754343506
tomoya <tomoya.kamaji@gmail.com>
2023-05-14 19:20:50 +0900

    階差数列

--- recursive/factorial.go
+++ recursive/factorial.go
@@ -2,9 +2,11 @@ package recursive
 
 // 階差数列を求める: 1, 2, 6, 24, 120, 720, 5040, 40320, 362880
 func factorial(n int) int {
+	// ベースケース
 	if n == 0 {
 		return 1
 	}
-	result := n * factorial(n-1)
-	return result
+
+	// 再帰呼び出し
+	return n * factorial(n-1)
 }
